60adbce56faf8afa0f5033141f8f8aca5c7f52ad
andreibeetlol <indoilaandreimarian@gmail.com>
2025-10-30 17:10:19 +0200

    blabla

---
+++ Lifetime/Program.cs
@@ -0,0 +1,43 @@
+﻿static class Program
+{
+    static async Task Main(string[] args)
+    {
+        var serviceCollection = new ServiceCollection();
+        var type = Console.ReadLine();
+
+        switch (type)
+        {
+            case "singleton":
+                serviceCollection.AddSingleton<ILifetimeTest, LifetimeTest>();
+                break;
+            case "scoped":
+                serviceCollection.AddScoped<ILifetimeTest, LifetimeTest>();
+                break;
+            default:
+                serviceCollection.AddTransient<ILifetimeTest, LifetimeTest>();
+                break;
+        }
+        
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        
+        Console.WriteLine("Testing with default scope:");
+        
+        for (var i = 0; i < 3; ++i)
+        {
+            var lifeTimeTest = serviceProvider.GetRequiredService<ILifetimeTest>();
+            
+            Console.WriteLine("\tThe returned ID is: {0}", lifeTimeTest.InstanceId);
+        }
+        
+        Console.WriteLine("Testing with multiple scopes:");
+        
+        for (var i = 0; i < 3; ++i)
+        {
+            using var scope = serviceProvider.CreateScope();
+            
+            var lifeTimeTest = scope.ServiceProvider.GetRequiredService<ILifetimeTest>();
+            
+            Console.WriteLine("\tThe returned ID is: {0}", lifeTimeTest.InstanceId);
+        }
+    }
+}
---
+++ observable_ceva/Program.cs
@@ -0,0 +1,32 @@
+﻿static class Program
+{
+    static async Task Main(string[] args)
+    {        
+        var cancellationTokenSource = new CancellationTokenSource();
+        var cancellationToken = cancellationTokenSource.Token;
+
+        // TODO: Creati obiectele pentru observabil si observator, apoi abonati observatorul
+        
+        var read = Task.Run(() =>
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var line = Console.ReadLine();
+
+                if (line == "exit")
+                {
+                    cancellationTokenSource.Cancel();
+                }
+                else
+                {
+                    // TODO: Trimiteti linia citita catre obsevabil 
+                }
+            }
+            
+        }, CancellationToken.None);
+        
+        await read;
+        
+        // TODO: Printati continutul din observator
+    }
+}
