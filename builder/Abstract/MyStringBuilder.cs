552bccfebb8808b0e95c0f71eb211c47ad2fe6bd
andreibeetlol <indoilaandreimarian@gmail.com>
2025-10-30 17:24:42 +0200

    milsu

---
+++ builder/Abstract/MyStringBuilder.cs
@@ -0,0 +1,20 @@
+﻿public class MyStringBuilder
+{
+    // Avem continutul pe care vrem sa-l modificam
+    private string _content = "";
+
+    // In acest caz avem nevoie doar de o singura primitiva de modificat continutul si
+    // observati ca returnam instanta proprie
+    public MyStringBuilder Add(string value)
+    {
+        _content += value;
+        
+        return this;
+    }
+
+    // Actiunea de constructie trebuie sa ne returneze rezultatul dorit
+    public string Build()
+    {
+        return _content;
+    }
+}
---
+++ builder/Abstract/MyStringBuilderExtensions.cs
@@ -0,0 +1,8 @@
+﻿public static class MyStringBuilderExtensions
+{
+    // Putem folosi primitive in metodele de extensie si sa implementam metode pentru necesitati specifice cu denumiri sugestive
+    public static MyStringBuilder AddNewLine(this MyStringBuilder builder)
+    {
+        return builder.Add("\r\n");
+    }
+}
